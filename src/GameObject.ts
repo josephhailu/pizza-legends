eb40e56452a6dbb63e7a526004cf8c6b8ab077a0
josephhailu <46454734+josephhailu@users.noreply.github.com>
2021-12-21 19:55:58 -0500

    init game object and sprite

---
+++ src/GameObject.ts
@@ -0,0 +1,20 @@
+type GameObjectConfig = {
+    x: number;
+    y: number;
+    src?: string;
+}
+
+class GameObject {
+    y: number
+    x: number
+    sprite: any;
+    constructor(config: GameObjectConfig) {
+        this.x = config.x || 0;
+        this.y = config.y || 0;
+        this.sprite = new Sprite({
+            gameObject: this,
+            src: config.src || "./images/characters/people/hero.png",
+            useShadow: true
+        });
+    }
+}
