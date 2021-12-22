6f8d1348de7703904ce5180912d463834a4c69f0
josephhailu <46454734+josephhailu@users.noreply.github.com>
2021-12-21 19:55:40 -0500

    update project structure

--- init.ts
+++
@@ -1,6 +0,0 @@
-(function (){
-    let overworld = new Overworld({
-        element: document.querySelector(".game-container")
-    });
-    overworld.init();
-})();
---
+++ src/Overworld.ts
@@ -0,0 +1,47 @@
+type OverworldConfig = {
+    element: HTMLElement;
+
+}
+
+
+class Overworld {
+    element: HTMLElement;
+    canvas: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+    constructor(config: OverworldConfig) {
+        this.element = config.element
+        this.canvas = this.element.querySelector(".game-canvas")!;
+        this.ctx = this.canvas.getContext("2d")!;
+    }
+
+    /**
+     * Draw to the canvas
+     * 
+     * create a new image, assign a  source to that image
+     * when that image is downloaded, we copy the info to the canvas
+     * 
+     * the image is scaled via css
+     */
+    init() {
+        const image = new Image();
+        image.onload = () => {
+            this.ctx.drawImage(image, 0, 0)
+        };
+        image.src = "./images/maps/DemoLower.png"
+
+        // place game objects
+        const hero = new GameObject({
+            x: 5,
+            y: 6,
+        })
+
+        const npc1 = new GameObject({
+            x: 5,
+            y: 6,
+            src: "./images/characters/people/npc1.png"
+        })
+
+        hero.sprite.draw(this.ctx)
+        npc1.sprite.draw(this.ctx)
+    }
+}
