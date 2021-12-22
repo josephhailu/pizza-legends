662d064ee899820f3419b9862c9470a8bfe87b64
josephhailu <46454734+josephhailu@users.noreply.github.com>
2021-12-21 21:09:36 -0500

    map configs in OverworldMap

--- src/OverworldMap.ts
+++ src/OverworldMap.ts
@@ -30,4 +30,42 @@ class OverworldMap {
         ctx.drawImage(this.upperImage, 0, 0)
     }
 }
- 
+
+export { };
+declare global {
+    interface Window { OverworldMaps: any; }
+}
+
+window.OverworldMaps = {
+    DemoRoom: {
+        lowerSrc: "images/maps/DemoLower.png",
+        upperSrc: "images/maps/DemoUpper.png",
+        gameObjects: {
+            hero: new GameObject({
+                x: 5,
+                y: 6
+            }),
+            npc1: new GameObject({
+                x: 7,
+                y: 9,
+                src: "./images/characters/people/npc1.png"
+            })
+        }
+    },
+    Kitchen: {
+        lowerSrc: "images/maps/KitchenLower.png",
+        upperSrc: "images/maps/KitchenUpper.png",
+        gameObjects: {
+            npcA: new GameObject({
+                x: 9,
+                y: 2,
+                src: "./images/characters/people/npc2.png"
+            }),
+            npcB: new GameObject({
+                x: 10,
+                y: 4,
+                src: "./images/characters/people/npc3.png"
+            })
+        }
+    }
+};
