fc1a8ab00be87ef5aa7f8db70760434c3f0ee286
josephhailu <46454734+josephhailu@users.noreply.github.com>
2021-12-21 21:41:15 -0500

    remove map drawing logic

--- src/Overworld.ts
+++ src/Overworld.ts
@@ -8,6 +8,7 @@ class Overworld {
     element: HTMLElement;
     canvas: HTMLCanvasElement;
     ctx: CanvasRenderingContext2D;
+    map?: OverworldMap = undefined;
     constructor(config: OverworldConfig) {
         this.element = config.element
         this.canvas = this.element.querySelector(".game-canvas")!;
@@ -34,30 +35,8 @@ class Overworld {
      * the image is scaled via css
      */
     init() {
+        this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
         this.startGameLoop();
-
-        const image = new Image();
-        image.onload = () => {
-            this.ctx.drawImage(image, 0, 0)
-        };
-        image.src = "./images/maps/DemoLower.png"
-
-        // place game objects
-        const hero = new GameObject({
-            x: 5,
-            y: 6,
-        })
-
-        const npc1 = new GameObject({
-            x: 7,
-            y: 9,
-            src: "./images/characters/people/npc1.png"
-        })
-
-        setTimeout(() => {
-            hero.sprite.draw(this.ctx)
-            npc1.sprite.draw(this.ctx)
-        }, 200);
 
     }
 
