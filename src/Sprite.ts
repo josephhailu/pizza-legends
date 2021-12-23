bf5dc035b544cee1686ea1ea7e4d7b0783566d31
josephhailu <46454734+josephhailu@users.noreply.github.com>
2021-12-22 21:12:57 -0500

    main character animation based on movement

--- src/Sprite.ts
+++ src/Sprite.ts
@@ -1,15 +1,20 @@
 type SpriteConfig = {
-    animation?: { idleDown: number[][]; };
+    animation?: {
+        [key: string]: number[][];
+    };
     currentAnimation?: string;
     currentAnimationFrame?: number;
     src: string;
     gameObject: GameObject;
     useShadow: boolean;
+    animationFrameLimit?: number;
 };
 
 class Sprite {
-    animation: any;
-    currentAnimation: any;
+    animation: {
+        [key: string]: number[][];
+    };
+    currentAnimation: string;
     currentAnimationFrame: number;
     image: HTMLImageElement;
     isLoaded: boolean = false;
@@ -17,6 +22,8 @@ class Sprite {
     shadow: HTMLImageElement;
     useShadow: boolean;
     isShadowLoaded: boolean = false;
+    animationFrameLimit: number;
+    animationFrameProgress: number;
 
     constructor(config: SpriteConfig) {
 
@@ -39,15 +46,51 @@ class Sprite {
 
         // animation and initital state
         this.animation = config.animation || {
-            idleDown: [
-                [0, 0]
-            ]
+            'idle-down': [[0, 0]],
+            'idle-right': [[0, 1]],
+            'idle-up': [[0, 2]],
+            'idle-left': [[0, 3]],
+
+            'walk-down': [[1, 0], [0, 0], [3, 0], [0, 0]],
+            'walk-right': [[1, 1], [0, 1], [3, 1], [0, 1]],
+            'walk-up': [[1, 2], [0, 2], [3, 2], [0, 2]],
+            'walk-left': [[1, 3], [0, 3], [3, 3], [0, 3]],
         }
-        this.currentAnimation = config.currentAnimation || 'idleDown'
+        this.currentAnimation = 'idle-down'//config.currentAnimation || 'idle-down'
         this.currentAnimationFrame = 0;
+
+        this.animationFrameLimit = config.animationFrameLimit || 16;
+        this.animationFrameProgress = this.animationFrameLimit;
 
         //reference game object (the thing that created this spirte)
         this.gameObject = config.gameObject;
+    }
+
+    get frame() {
+        return this.animation[this.currentAnimation][this.currentAnimationFrame];
+    }
+
+
+    setAnimation(key: string) {
+        if (this.currentAnimation !== key) {
+            this.currentAnimation = key
+            this.currentAnimationFrame = 0
+            this.animationFrameProgress = this.animationFrameLimit
+        }
+    }
+
+    updateAnimationProgress() {
+        if (this.animationFrameProgress > 0) {
+            this.animationFrameProgress -= 1;
+            return;
+        }
+        //reset counter
+        this.animationFrameProgress = this.animationFrameLimit
+        this.currentAnimationFrame += 1
+        if (this.frame === undefined) {
+            this.currentAnimationFrame = 0;
+        }
+
     }
 
     draw(ctx: CanvasRenderingContext2D) {
@@ -61,13 +104,16 @@ class Sprite {
                 x, y, // starting crop point for top/left
             )
 
+        const [frameX, frameY] = this.frame;
+
         this.isLoaded &&
-            ctx.drawImage(
-                this.image,
-                0, 0, // starting crop point for top/left
+            ctx.drawImage(this.image,
+                frameX * 32, frameY * 32, // starting crop point for top/left
                 32, 32, // width/height of crop to make
                 x, y,
                 32, 32 // image width/height (based on character size in sprite sheet)
             )
+
+        this.updateAnimationProgress();
     }
 }
