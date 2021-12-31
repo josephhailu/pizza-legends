942ce74454cde37ed57f999b74c68f9187765322
josephhailu <46454734+josephhailu@users.noreply.github.com>
2021-12-30 21:52:19 -0500

    initial text message logic and css refactor

---
+++ src/TextMessage.ts
@@ -0,0 +1,29 @@
+interface TextMessageParams {
+  text: string;
+  onComplete: () => void;
+}
+class TextMessage {
+  text: string;
+  element: HTMLElement | null;
+  onComplete: () => void;
+  constructor({ text, onComplete }: TextMessageParams) {
+    this.text = text;
+    this.onComplete = onComplete;
+    this.element = null;
+  }
+
+  createElement() {
+    this.element = document.createElement("div");
+    this.element.classList.add("TextMessage");
+
+    this.element.innerHTML = `
+      <p class ="TextMessage_p">${this.text}</p>
+      <button class ="TextMessage_button">Next</button
+      `;
+  }
+
+  init(container: HTMLElement) {
+    this.createElement();
+    container.appendChild(this.element!);
+  }
+}
