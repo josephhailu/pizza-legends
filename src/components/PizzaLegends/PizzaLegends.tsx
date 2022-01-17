4764a7db287e86632feb2e36a8874a7e3b940e7c
josephhailu <46454734+josephhailu@users.noreply.github.com>
2022-01-16 21:04:26 -0500

    wip: trying out cleanup effect to reset game

--- src/components/PizzaLegends/PizzaLegends.tsx
+++ src/components/PizzaLegends/PizzaLegends.tsx
@@ -1,31 +1,84 @@
-import {useEffect, useRef} from "react";
+import {useEffect, useMemo, useState, useCallback} from "react";
 import {DIMENSIONS} from "../utils";
-import "../../styles/index.css";
 import "../../styles/TextMessage.css";
 
 import Overworld from "../Overworld";
+import {StyledGameContainer} from "./styles";
 
 export default function PizzaLegends() {
-  const gameContainerRef = useRef<HTMLDivElement | null>(null);
-  const gameCanvasRef = useRef<HTMLCanvasElement | null>(null);
-  
-  // Init
-  useEffect(() => {
-    if (gameContainerRef.current && gameCanvasRef.current) {
-      gameCanvasRef.current.width = DIMENSIONS.canvasDimensions.width;
-      gameCanvasRef.current.height = DIMENSIONS.canvasDimensions.height;
-      const ov = new Overworld({
-        gameContainer: gameContainerRef.current,
-        canvas: gameCanvasRef.current,
-      });
-      ov.init();
+  const [gameContainer, setGameContainer] = useState<HTMLDivElement | null>(
+    null
+  );
+  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
+
+  const gameContainerRef = useCallback((div: HTMLDivElement | null) => {
+    if (div !== null) {
+      setGameContainer(div);
     }
   }, []);
+
+  const gameCanvasRef = useCallback((canvas: HTMLCanvasElement | null) => {
+    if (canvas !== null) {
+      setCanvas(canvas);
+    }
+  }, []);
+  // const [overworld, setOverworld] = useState<Overworld | null>(null);
+
+  // Init
+  useEffect(() => {
+    if (gameContainer && canvas) {
+      canvas.width = DIMENSIONS.canvasDimensions.width;
+      canvas.height = DIMENSIONS.canvasDimensions.height;
+      //setOverworld(new Overworld({gameContainer, canvas}));
+    }
+    return () => {
+      if (gameContainer && canvas) {
+        setGameContainer(null);
+        setCanvas(null);
+      }
+    };
+  }, [gameContainer, canvas]);
+
+  // Init
+  // useEffect(() => {
+  //   if (overworld) {
+  //     overworld.init();
+  //   }
+  //   return () => {
+  //     if (overworld) {
+  //       overworld.stop();
+  //     }
+  //   };
+  // }, [overworld]);
+
+  const stableOverworldRef = useMemo(() => {
+    if (gameContainer && canvas) {
+      console.log("initing");
+      return new Overworld({
+        gameContainer,
+        canvas,
+      });
+    }
+    console.log("nulling");
+    return null;
+  }, [gameContainer, canvas]);
+
+  // cleanup
+  useEffect(() => {
+    if (stableOverworldRef) {
+      stableOverworldRef.init();
+    }
+
+    return () => {
+      if (stableOverworldRef) {
+        stableOverworldRef.stop();
+      }
+    };
+  }, [stableOverworldRef]);
+
   return (
-    <main>
-      <div className="game-container" ref={gameContainerRef}>
-        <canvas className="game-canvas" ref={gameCanvasRef} />
-      </div>
-    </main>
+    <StyledGameContainer className="game-container" ref={gameContainerRef}>
+      <canvas className="game-canvas" ref={gameCanvasRef} />
+    </StyledGameContainer>
   );
 }
